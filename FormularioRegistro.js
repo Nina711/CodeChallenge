80cfccacbc9c548aa18ee76b502c585499c7cffc
Nina711 <curbelonobleromina@gmail.com>
2023-09-04 21:04:52 -0300

    Challenge
    Tarea de clase

---
+++ FormularioRegistro.js
@@ -0,0 +1,27 @@
+function submitForm(){
+    const nombre = document.getElementById("fname").value;
+    const apellido = document.getElementById("lname").value;
+    const fechaDeNacimiento = document.getElementById("bday").value;
+
+    const formData = {
+        nombre: nombre,
+        apellido: apellido,
+        fechaDeNacimiento: fechaDeNacimiento
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/users", {
+        method: 'POST',
+        headers:{
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+
+    .then (Response => Response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
